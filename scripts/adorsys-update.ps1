f0664376f62dfec24a8716e0846055a421c492d9
t-desmond <desmondtardzenyuy@gmail.com>
2026-02-24 16:25:49 +0100

    chore: update wazuh agent version -> 4.14.2-1

--- scripts/adorsys-update.ps1
+++ scripts/adorsys-update.ps1
@@ -25,7 +25,7 @@ Set-StrictMode -Version Latest
 # ---- Configuration Variables ----
 $LOG_LEVEL = if ($env:LOG_LEVEL) { $env:LOG_LEVEL } else { "INFO" }
 $WAZUH_MANAGER = if ($env:WAZUH_MANAGER) { $env:WAZUH_MANAGER } else { "wazuh.example.com" }
-$WAZUH_AGENT_VERSION = if ($env:WAZUH_AGENT_VERSION) { $env:WAZUH_AGENT_VERSION } else { "4.13.1-1" }
+$WAZUH_AGENT_VERSION = if ($env:WAZUH_AGENT_VERSION) { $env:WAZUH_AGENT_VERSION } else { "4.14.2-1" }
 $OSSEC_PATH = "C:\Program Files (x86)\ossec-agent\"
 $OSSEC_CONF_PATH = Join-Path -Path $OSSEC_PATH -ChildPath "ossec.conf"
 $RepoUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/main"
