b6752d831752af621c9ee87d6516fcb9d712544a
Awambeng Rodrick <awambengrodrick@gmail.com>
2026-05-12 07:59:09 +0100

    chore: bump application version to 0.5.0-rc.2 and update release workflow configuration
    Signed-off-by: Awambeng Rodrick <awambengrodrick@gmail.com>

--- scripts/macos/install.sh
+++ scripts/macos/install.sh
@@ -7,7 +7,7 @@ else
     set -eu
 fi
 
-APP_VERSION=${APP_VERSION:-"0.5.0-rc.1"}
+APP_VERSION=${APP_VERSION:-"0.5.0-rc.2"}
 
 # Common configuration
 SERVER_NAME=${SERVER_NAME:-"wazuh-agent-status"}
