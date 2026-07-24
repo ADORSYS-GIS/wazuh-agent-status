73835abb0e3269fd6b10a30636952e638ba94dd3
mbiti2 <louisembiti@gmail.com>
2026-07-24 10:05:02 +0100

    chore: bump application version to 0.5.1-rc.1

--- scripts/windows/adorsys-update.ps1
+++ scripts/windows/adorsys-update.ps1
@@ -39,7 +39,7 @@ if (-not $IsAdmin) {
 
 # Configuration
 
-$APP_VERSION = if ($env:APP_VERSION) { $env:APP_VERSION } else { "0.5.1" }
+$APP_VERSION = if ($env:APP_VERSION) { $env:APP_VERSION } else { "0.5.1-rc.1" }
 $REPO_REF = if ($env:WAZUH_AGENT_STATUS_REPO_REF) { $env:WAZUH_AGENT_STATUS_REPO_REF } else { "user-main" }
 $REPO_URL = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent-status/$REPO_REF"
 $AGENT_REPO_REF = if ($env:WAZUH_AGENT_REPO_REF) { $env:WAZUH_AGENT_REPO_REF } else { "main" }
