1f7c8cf47b41062edcbac659b2b60663ef85e89c
mbiti2 <louisembiti@gmail.com>
2026-09-03 08:52:58 +0100

    chore: bump application version to 0.5.3 across all installation and update scripts

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -13,7 +13,7 @@ if [[ "$(uname -s)" != "Linux" ]]; then
     exit 1
 fi
 
-APP_VERSION=${APP_VERSION:-"0.5.2"}
+APP_VERSION=${APP_VERSION:-"0.5.3"}
 
 # Common configuration
 WAZUH_AGENT_STATUS_REPO_REF=${WAZUH_AGENT_STATUS_REPO_REF:-"main"}
